6f276dd5080766b9989f5b8371ce42fac1a36e21
ligl <ligl@avic-digital.com/avicit>
2019-12-27 14:42:08 +0800

    Initial commit

---
+++ src/main/java/com/ant/msgerall/AntMsgerApplication.java
@@ -0,0 +1,13 @@
+package com.ant.msgerall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntMsgerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AntMsgerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ant/msgerall/AntMsgerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ant.msgerall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AntMsgerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
